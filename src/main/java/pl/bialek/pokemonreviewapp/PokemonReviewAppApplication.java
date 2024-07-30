6d2621f9248f5d0572180755cac253d452fea30a
Michał Białek <kontakt.michalbialek@gmail.com>
2024-07-30 17:25:02 +0200

    Initial commit - basic db configuration and adding required packages

---
+++ src/main/java/pl/bialek/pokemonreviewapp/PokemonReviewAppApplication.java
@@ -0,0 +1,13 @@
+package pl.bialek.pokemonreviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonReviewAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonReviewAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bialek/pokemonreviewapp/PokemonReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bialek.pokemonreviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonReviewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
