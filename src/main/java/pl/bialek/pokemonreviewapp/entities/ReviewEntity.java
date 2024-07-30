a339395a645af4990a601c1c0edecb0e4050e37e
Michał Białek <kontakt.michalbialek@gmail.com>
2024-07-30 17:32:03 +0200

    Added PokemonEntity and ReviewEntity.

---
+++ src/main/java/pl/bialek/pokemonreviewapp/entities/ReviewEntity.java
@@ -0,0 +1,26 @@
+package pl.bialek.pokemonreviewapp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class ReviewEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private String content;
+    private byte stars;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pokemon_id")
+    private PokemonEntity pokemon;
+}
